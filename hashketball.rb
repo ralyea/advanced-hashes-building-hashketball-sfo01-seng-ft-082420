ed2d02be05624ec6dfad5795fa9982bbc78d5e19
Paul Ralyea <047par@gmail.com>
2020-05-13 00:07:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -22,5 +22,6 @@ def game_hash()
   { :player_name => "Kemba Walker", :number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 7, :blocks => 5, :slam_dunks => 12 }
 ]
 }
+}
 end
     
