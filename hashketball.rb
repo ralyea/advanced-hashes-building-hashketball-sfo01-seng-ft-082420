079c8d2f2fdddbeb39d07aececbe5aac145c62ae
Paul Ralyea <047par@gmail.com>
2020-05-13 00:19:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,8 +11,8 @@ def game_hash
   {:player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1 }
   ]
   }
-  }
-  {:away=>
+  },
+   :away=>
   {:team_name=>"Charlotte Hornets",
   :colors=>["Turquoise, Purple"],
   :players=>[
